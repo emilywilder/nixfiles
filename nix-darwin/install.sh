b84448e460be346010bb2ce384d471bee3819011
Emily Lockhart <validitymaiden@gmail.com>
2025-05-04 18:32:32 -0400

    rewrite install.sh to grab the build target from a provided nix file

--- nix-darwin/install.sh
+++ nix-darwin/install.sh
@@ -1,3 +1,41 @@
-#!/bin/sh
+#!/usr/bin/env zsh
+# needs string sub
 
-nix run nix-darwin/master#darwin-rebuild -- switch
+# unquote("string") -> string
+unquote() {
+    local -r _arg=${1:?}
+    echo ${_arg//\"/}
+}
+
+# getNixDarwinUrl(nixFile) -> nixDarwinUrl
+getNixDarwinUrl() {
+    nix eval -f ${1:?} inputs.nix-darwin.url
+    return $?
+}
+
+# getRepoFromUrl(nixDarwinUrl) -> repo
+getRepoFromUrl() {
+    local -r _arg=${1:?}
+    echo ${_arg#*:*/}
+}
+
+# getRebuildTargetFromRepo(repo) -> rebuildTarget
+getRebuildTargetFromRepo() {
+    echo "${1:?}#darwin-rebuild"
+}
+
+# initNixDarwin(rebuildTarget) -> null
+initNixDarwin() {
+    nix run ${1:?} -- switch
+    return $?
+}
+
+initNixDarwin $(
+    getRebuildTargetFromRepo $(
+        getRepoFromUrl $(
+            unquote $(
+                getNixDarwinUrl ${1:?no nix file given}
+            )
+        )
+    )
+)
