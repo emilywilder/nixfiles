12e820cb2d9072b91168d3ccb33bd3e01a304374
Emily Lockhart <validitymaiden@gmail.com>
2025-05-13 17:42:39 -0400

    overwrite athena_flake.nix with freshly created flake from nix-darwin-config

--- nix-darwin/athena_flake.nix
+++ nix-darwin/athena_flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "nix-darwin system flake";
+  description = "Example nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -27,7 +27,7 @@
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+      system.stateVersion = 5;
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
