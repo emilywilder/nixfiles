ba087f05078e7549c681fafac63c1933cb8a27d7
Emily Lockhart <validitymaiden@gmail.com>
2025-05-19 19:02:55 -0400

    add nix files for nixos

---
+++ nixos/iso.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  boot.kernelModules = [ "wl" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.blacklistedKernelModules = [ "b43" "bcma" ];
+}
