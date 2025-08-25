129097911d3497544b7b898734703e84ab8e0928
Emily Lockhart <validitymaiden@gmail.com>
2025-08-25 17:21:42 -0400

    nixos: - add packages to home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -16,6 +16,50 @@
   # changes in each release.
   home.stateVersion = "25.05";
 
+  home.packages = [
+    # utilities
+    pkgs.coreutils
+    pkgs.file
+    pkgs.sourceHighlight
+    pkgs.bat
+    pkgs.tree
+    pkgs.pdfgrep
+    pkgs.xz
+    pkgs._7zz
+    pkgs.jq
+    pkgs.shfmt
+    pkgs.shellcheck
+    pkgs.ripgrep
+    pkgs.eza
+    pkgs.stow
+    pkgs.wipe
+    # editors
+    pkgs.vim
+    pkgs.neovim
+    # terminal emulators and multiplexers
+    pkgs.tmux
+    # scm
+    pkgs.git
+    # languages
+    pkgs.python313
+    pkgs.python313Packages.pip
+    pkgs.perl
+    pkgs.nodejs
+    pkgs.R
+    pkgs.typescript
+    # build tools
+    pkgs.bmake
+    pkgs.gnumake
+    pkgs.cmake
+    pkgs.yarn
+    pkgs.just
+    # networking utilities
+    pkgs.wget
+    pkgs.socat
+    pkgs.openssh
+    pkgs.nmap
+  ];
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
