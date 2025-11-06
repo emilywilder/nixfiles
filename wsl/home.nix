3a2cbd5f3ea171ea59d47b551849a8acf1d683f7
Emily Lockhart <validitymaiden@gmail.com>
2025-11-06 17:57:47 -0500

    build(wsl): replace home.packages
    - Replaced home.packages from example with a slightly curated list from my nix-darwin home.nix

--- wsl/home.nix
+++ wsl/home.nix
@@ -8,8 +8,65 @@
 
   # Packages that should be installed to the user profile.
   home.packages = [
-    pkgs.htop
-    pkgs.fortune
+    # utilities
+    pkgs.coreutils
+    pkgs.file
+    pkgs.sourceHighlight
+    pkgs.bat
+    pkgs.tree
+    pkgs.pdfgrep
+    pkgs.xz
+    pkgs._7zz
+    pkgs.jq
+    pkgs.shfmt
+    pkgs.graphviz
+    pkgs.shellcheck
+    pkgs.ripgrep
+    pkgs.eza
+    pkgs._1password-cli
+    pkgs.stow
+    pkgs.wipe
+    pkgs.fzf
+    pkgs.atuin
+    pkgs.glow # cli markdown viewer
+    # editors
+    pkgs.vim
+    pkgs.neovim
+    # terminal emulators and multiplexers
+    pkgs.tmux
+    # scm
+    pkgs.git
+    # languages
+    pkgs.python313
+    pkgs.python313Packages.pip
+    pkgs.perl
+    pkgs.nodejs
+    pkgs.R
+    pkgs.typescript
+    pkgs.racket
+    # build tools
+    pkgs.bmake
+    pkgs.gnumake
+    pkgs.cmake
+    pkgs.yarn
+    pkgs.just
+    # networking utilities
+    pkgs.wget
+    pkgs.socat
+    pkgs.openssh
+    pkgs.nmap
+    # databases
+    pkgs.sqlite
+    pkgs.postgresql
+    # emulators / virtualization
+    pkgs.qemu
+    pkgs.podman
+    pkgs.podman-compose
+    # graphically enabled cli tools
+    pkgs.neofetch # generate system info
+    pkgs.tdf  # view pdfs
+    pkgs.yazi # split file manager
+    pkgs.presenterm # view markdown
   ];
 
   # This value determines the Home Manager release that your
