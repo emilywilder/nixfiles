9cdb295488af4874777b76d692af43f18bf87906
Emily Lockhart <validitymaiden@gmail.com>
2025-11-06 17:36:26 -0500

    fix(wsl): add nixos-wsl to flake inputs and outputs

--- wsl/flake.nix
+++ wsl/flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-wsl, ... }: {
     nixosConfigurations = {
       iris-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
