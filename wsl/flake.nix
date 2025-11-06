ecaa9033c944427301c1ef700cc2f9f170cbf235
Emily Lockhart <validitymaiden@gmail.com>
2025-11-06 14:32:52 -0500

    build(wsl): update flake hostname and formatting
    - add wsl nixos hostname this will be used for
    - nest some of the config defs
    - update description

--- wsl/flake.nix
+++ wsl/flake.nix
@@ -1,24 +1,27 @@
 {
-  description = "NixOS configuration";
+  description = "WSL NixOS flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
+      iris-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.emily = ./home.nix;
-
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.emily = ./home.nix;
+            };
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
