0252c9fe3ad81ea243eca3d0a3e4ac2fcb3fa113
Emily Lockhart <validitymaiden@gmail.com>
2025-11-06 20:17:57 -0500

    feat(wsl): add automatic gc and store optimization

--- wsl/configuration.nix
+++ wsl/configuration.nix
@@ -74,4 +74,18 @@
   hardware.graphics = {
     enable = true;
   };
+
+  # Perform garbage collection weekly to maintain low disk usage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
 }
