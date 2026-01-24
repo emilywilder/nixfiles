81d477c9a3645e0e625b63035f3486e54f1dfa67
Emily Lockhart <validitymaiden@gmail.com>
2026-01-24 14:54:40 -0500

    feat(nixos): don't emulate x86_64 in vm flake

--- nixos/vm-configuration.nix
+++ nixos/vm-configuration.nix
@@ -10,7 +10,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
+  #boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
 
   services.getty.helpLine = "\nenp0s1:\n\tIPv4:\t\\4{enp0s1}\n\tIPv6:\t\\6{enp0s1}";
 
