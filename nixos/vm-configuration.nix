b84688ae9e79c2d188eb9971d193559759413257
Emily Lockhart <validitymaiden@gmail.com>
2026-01-27 16:36:08 -0500

    feat(nixos): enable nix-ld for vscode remote ssh

--- nixos/vm-configuration.nix
+++ nixos/vm-configuration.nix
@@ -9,6 +9,9 @@
 
   networking.hostName = "nixos-vm";
 
+  # allow vscode to connect to VM
+  programs.nix-ld.enable = true;
+
   #boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
 
   services.getty.helpLine = "\nenp0s1:\n\tIPv4:\t\\4{enp0s1}\n\tIPv6:\t\\6{enp0s1}";
