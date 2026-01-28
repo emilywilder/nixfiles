fff3d445c94e9c0eaa3697f742e891a49e0f1a7c
Emily Lockhart <validitymaiden@gmail.com>
2026-01-28 16:21:33 -0500

    refactor(nixos): use directory/default.nix pattern

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,12 +15,12 @@
         # use specialArgs to pass inputs to the configuration
         nixos-vm = nixpkgs.lib.nixosSystem {
           system = "aarch64-linux";
-          modules = [ ./UTM/vm-configuration.nix ];
+          modules = [ ./UTM ];
           specialArgs = { inherit inputs; };
         };
         nixos-macbook = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [ ./MacBookPro14_2/macbook-configuration.nix ];
+          modules = [ ./MacBookPro14_2 ];
           specialArgs = { inherit inputs; };
         };
       };
