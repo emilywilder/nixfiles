3d446c64741fe492144b8fe80788df5e79dd0030
Emily Lockhart <validitymaiden@gmail.com>
2026-02-07 13:01:43 -0500

    style(iris-nixos): reformat flake

--- hosts/iris-nixos/flake.nix
+++ hosts/iris-nixos/flake.nix
@@ -1,29 +1,25 @@
 {
-      self,
-      nixpkgs,
-      home-manager,
-      nixos-wsl,
-      ...
-    }:
+  self,
+  nixpkgs,
+  home-manager,
+  nixos-wsl,
+  ...
+}:
+{
+  modules = [
+    # Set Git commit hash for version.
+    { system.configurationRevision = self.rev or self.dirtyRev or null; }
+    nixos-wsl.nixosModules.wsl
+    ./configuration.nix
+    home-manager.nixosModules.home-manager
     {
-      
-        
-          modules = [
-            # Set Git commit hash for version.
-            { system.configurationRevision = self.rev or self.dirtyRev or null; }
-            nixos-wsl.nixosModules.wsl
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.emily = ./home.nix;
-              };
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to home.nix
-            }
-          ];
-        
-      
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.emily = ./home.nix;
+      };
+      # Optionally, use home-manager.extraSpecialArgs to pass
+      # arguments to home.nix
     }
+  ];
+}
