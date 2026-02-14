0b31c87993d79c1546179fa2693d3e8bdb36c105
Emily Lockhart <validitymaiden@gmail.com>
2026-02-14 15:15:39 -0500

    refactor(wsl): factor home-manager into its own function

--- modules/wsl/default.nix
+++ modules/wsl/default.nix
@@ -8,15 +8,6 @@
     { system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null; }
     inputs.nixos-wsl.nixosModules.wsl
     ./configuration.nix
-    inputs.home-manager.nixosModules.home-manager
-    {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.emily = ./home.nix;
-      };
-      # Optionally, use home-manager.extraSpecialArgs to pass
-      # arguments to home.nix
-    }
+    ./home-manager.nix
   ];
 }
