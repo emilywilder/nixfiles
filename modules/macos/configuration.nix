fc091ed95987d01b2f42805571570cd64cfa0f65
Emily Lockhart <validitymaiden@gmail.com>
2026-02-20 23:47:35 -0500

    refactor(macos): factor configuration

---
+++ modules/macos/configuration.nix
@@ -0,0 +1,61 @@
+{
+  inputs,
+  ...
+}:
+let
+  # followed method found from
+  # https://github.com/nix-community/home-manager/issues/6036#issuecomment-2661394278
+  username = "emily";
+
+  configuration =
+    { pkgs, ... }:
+    {
+      users = {
+        users.${username} = {
+          home = "/Users/${username}";
+          name = "${username}";
+        };
+      };
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [
+        pkgs.vim
+        pkgs.tmux
+        #pkgs.nvim
+      ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Use lix
+      nix.package = pkgs.lix;
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # allow unfree packages
+      nixpkgs.config.allowUnfree = true;
+
+      # nix-darwin now needs to utilize a primary user as part of the
+      # transition from gloabl to user config
+      system.primaryUser = "emily";
+    };
+in
+# Build darwin flake using:
+# $ darwin-rebuild build --flake .#athena
+{
+  imports = [
+    configuration
+  ];
+}
--- modules/macos/default.nix
+++ modules/macos/default.nix
@@ -3,10 +3,6 @@
   ...
 }:
 let
-  # followed method found from
-  # https://github.com/nix-community/home-manager/issues/6036#issuecomment-2661394278
-  username = "emily";
-
   # As according to:
   # https://nixos.wiki/wiki/flakes#Importing_packages_from_multiple_channels
   # https://discourse.nixos.org/t/how-to-fix-evaluation-warning-system-has-been-renamed-to-replaced-by-stdenv-hostplatform-system/72120
@@ -16,51 +12,6 @@ let
   overlay-r2505 = final: prev: {
     r2505 = inputs.nixpkgs-r2505.legacyPackages.${prev.stdenv.hostPlatform.system};
   };
-
-  configuration =
-    { pkgs, ... }:
-    {
-      users = {
-        users.${username} = {
-          home = "/Users/${username}";
-          name = "${username}";
-        };
-      };
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [
-        pkgs.vim
-        pkgs.tmux
-        #pkgs.nvim
-      ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Use lix
-      nix.package = pkgs.lix;
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # allow unfree packages
-      nixpkgs.config.allowUnfree = true;
-
-      # nix-darwin now needs to utilize a primary user as part of the
-      # transition from gloabl to user config
-      system.primaryUser = "emily";
-    };
 in
 # Build darwin flake using:
 # $ darwin-rebuild build --flake .#athena
@@ -75,7 +26,7 @@ in
         ];
       }
     )
-    configuration
+    ./configuration.nix
     ./macos.nix
     ./home-manager.nix
   ];
