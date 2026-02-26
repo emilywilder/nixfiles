f90394d5cce1aa40f87bb141122e4d36c1e20acf
Emily Lockhart <validitymaiden@gmail.com>
2026-02-26 00:52:38 -0500

    style(options): reformat

--- modules/options.nix
+++ modules/options.nix
@@ -1,17 +1,22 @@
-{ config, lib, pkgs, ... }:
 {
-  options.my.username = lib.mkOption {
-    type = lib.types.str;
-    description = "The username of the main user.";
-  };
-
-  options.my.openssh.keys = lib.mkOption {
-    type = lib.types.listOf lib.types.str;
-    description = "List of authorized SSH keys.";
-  };
-
-  options.my.home.config = lib.mkOption {
-    type = lib.types.path;
-    description = "Path to the home-manager configuration for the main user.";
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.my = {
+    username = lib.mkOption {
+      type = lib.types.str;
+      description = "The username of the main user.";
+    };
+    openssh.keys = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "List of authorized SSH keys.";
+    };
+    home.config = lib.mkOption {
+      type = lib.types.path;
+      description = "Path to the home-manager configuration for the main user.";
+    };
   };
 }
