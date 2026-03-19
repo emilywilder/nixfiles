40d61b0ff55bef3c9ae13ab3b2d95fbc7759a381
Emily Lockhart <validitymaiden@gmail.com>
2026-03-19 17:56:32 -0400

    docs(home-manager): 🔥remove comments from home.nix files

--- hosts/iris-nixos/home.nix
+++ hosts/iris-nixos/home.nix
@@ -12,12 +12,10 @@
     (inputs.self + /modules/home)
   ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = my.username;
   home.homeDirectory = "/home/${my.username}";
+  home.stateVersion = "25.05";
 
-  # Packages that should be installed to the user profile.
   home.packages = [
     # utilities
     pkgs.coreutils
@@ -87,14 +85,4 @@
     # pkgs.jetbrains.idea-oss
     # pkgs.firefox
   ];
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "25.05";
 }
