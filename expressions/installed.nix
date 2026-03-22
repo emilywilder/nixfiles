8672c03738ae3f9a6120ccf606cf7cb19104fb3d
Emily Lockhart <validitymaiden@gmail.com>
2026-03-22 02:55:12 -0400

    feat(installed)!: 👨‍💻 provide revision data
    - Used git+file location to have access to system.configurationRevision
    - Put system and home package information into "packages" attr name.
    - Added "revisions" attr name, providing revisions.nixpkgs and revisions.configuration.

--- expressions/installed.nix
+++ expressions/installed.nix
@@ -1,12 +1,12 @@
 # inspired from https://www.reddit.com/r/NixOS/comments/fsummx/how_to_list_all_installed_packages_on_nixos/
 
 # can be invoked as such for system packages:
-#   nix eval -f installed.nix --json | jq .system
+#   nix eval -f installed.nix --json | jq .packages.system
 # or home-manager packages:
-#   nix eval -f installed.nix --json | jq .home
+#   nix eval -f installed.nix --json | jq .packages.home
 
 let
-  flake = builtins.getFlake "/Users/emily/src/nixfiles";
+  flake = builtins.getFlake "git+file:///Users/emily/src/nixfiles";
   config = flake.darwinConfigurations.athena.config;
   pkgs = flake.inputs.nixpkgs;
   getPackageNames = builtins.map (p: "${p.name}");
@@ -14,6 +14,13 @@ let
   packageNameList = pl: sortUnique (getPackageNames pl);
 in
 {
-  "system" = packageNameList config.environment.systemPackages;
-  "home" = packageNameList config.home-manager.users.emily.home.packages;
+  # not git revision of flake, just of nixpkgs
+  "revisions" = {
+    "nixpkgs" = config.system.nixpkgsRevision;
+    "configuration" = config.system.configurationRevision;
+  };
+  "packages" = {
+    "system" = packageNameList config.environment.systemPackages;
+    "home" = packageNameList config.home-manager.users.emily.home.packages;
+  };
 }
