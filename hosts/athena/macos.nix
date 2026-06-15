cb8d60ea5dc3a5eb0fe01cb461266f4a195e742c
Emily Lockhart <validitymaiden@gmail.com>
2026-06-15 18:28:14 -0400

    refactor(athena): ♻️ nest macos defaults

--- hosts/athena/macos.nix
+++ hosts/athena/macos.nix
@@ -2,53 +2,62 @@
 {
   # Whether to enable Touch ID with sudo.
   security.pam.services.sudo_local.touchIdAuth = true;
-  # Configures the trackpad tap behavior. Mode 1 enables tap to click.
-  system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = null;
 
-  #Use scroll gesture with the Ctrl (^) modifier key to zoom. The default is false.
-  #FIXME system.defaults.universalaccess.closeViewScrollWheelToggle = true;
+  system.defaults = {
+    NSGlobalDomain = {
+      # Configures the trackpad tap behavior. Mode 1 enables tap to click.
+      "com.apple.mouse.tapBehavior" = null;
 
-  # Whether to show external disks, hard drives, connected servers,
-  # and removable media on desktop.
-  system.defaults.finder.ShowExternalHardDrivesOnDesktop = true;
-  system.defaults.finder.ShowHardDrivesOnDesktop = false;
-  system.defaults.finder.ShowMountedServersOnDesktop = true;
-  system.defaults.finder.ShowRemovableMediaOnDesktop = true;
+      # Whether to enable moving window by holding anywhere on it like on Linux.
+      # The default is false.
+      NSWindowShouldDragOnGesture = true;
 
-  # Keep folders on top when sorting by name.
-  system.defaults.finder._FXSortFoldersFirst = true;
+      # Avoid creating .DS_Store files on network or USB volumes.
+      #FIXME "com.apple.desktopservices DSDontWriteNetworkStores" = true;
+      #FIXME "com.apple.desktopservices DSDontWriteUSBStores" = true;
 
-  # Change the default search scope. Use “SCcf” to default to current folder.
-  # The default is unset (“This Mac”).
-  system.defaults.finder.FXDefaultSearchScope = "SCcf";
+      # Don't autofill in Safari.
+      #FIXME "com.apple.Safari AutoFillFromAddressBook" = false;
+      #FIXME "com.apple.Safari AutoFillPasswords" = false;
+      #FIXME "com.apple.Safari AutoFillCreditCardData" = false;
+      #FIXME "com.apple.Safari AutoFillMiscellaneousForms" = false;
 
-  # Avoid creating .DS_Store files on network or USB volumes.
-  #FIXME system.defaults.NSGlobalDomain."com.apple.desktopservices DSDontWriteNetworkStores" = true;
-  #FIXME system.defaults.NSGlobalDomain."com.apple.desktopservices DSDontWriteUSBStores" = true;
+      # Enable Secure Keyboard Entry in Terminal.app
+      # See: https://security.stackexchange.com/a/47786/8918
+      #FIXME "com.apple.terminal SecureKeyboardEntry" = true;
+    };
 
-  # Don't autofill in Safari.
-  #FIXME system.defaults.NSGlobalDomain."com.apple.Safari AutoFillFromAddressBook" = false;
-  #FIXME system.defaults.NSGlobalDomain."com.apple.Safari AutoFillPasswords" = false;
-  #FIXME system.defaults.NSGlobalDomain."com.apple.Safari AutoFillCreditCardData" = false;
-  #FIXME system.defaults.NSGlobalDomain."com.apple.Safari AutoFillMiscellaneousForms" = false;
+    finder = {
+      # Whether to show external disks, hard drives, connected servers,
+      # and removable media on desktop.
+      ShowExternalHardDrivesOnDesktop = true;
+      ShowHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = true;
+      ShowRemovableMediaOnDesktop = true;
 
-  # The filesystem path to which screencaptures should be written.
-  #FIXME system.defaults.screencapture.location = "${HOME}/Pictures/Screenshots";
+      # Keep folders on top when sorting by name.
+      _FXSortFoldersFirst = true;
 
-  # Enable highlight hover effect for the grid view of a stack in the Dock.
-  system.defaults.dock.mouse-over-hilite-stack = true;
-  # Whether to group windows by application in Mission Control’s Exposé. The default is false.
-  system.defaults.dock.expose-group-apps = true;
-  # Show recent applications in the dock. The default is true.
-  system.defaults.dock.show-recents = false;
-  # Whether to minimize windows into their application icon. The default is false.
-  system.defaults.dock.minimize-to-application = true;
+      # Change the default search scope. Use “SCcf” to default to current folder.
+      # The default is unset (“This Mac”).
+      FXDefaultSearchScope = "SCcf";
+    };
 
-  # Enable Secure Keyboard Entry in Terminal.app
-  # See: https://security.stackexchange.com/a/47786/8918
-  #FIXME system.defaults.NSGlobalDomain."com.apple.terminal SecureKeyboardEntry" = true;
+    dock = {
+      # Enable highlight hover effect for the grid view of a stack in the Dock.
+      mouse-over-hilite-stack = true;
+      # Whether to group windows by application in Mission Control’s Exposé. The default is false.
+      expose-group-apps = true;
+      # Show recent applications in the dock. The default is true.
+      show-recents = false;
+      # Whether to minimize windows into their application icon. The default is false.
+      minimize-to-application = true;
+    };
 
-  # Whether to enable moving window by holding anywhere on it like on Linux.
-  # The default is false.
-  system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+    #Use scroll gesture with the Ctrl (^) modifier key to zoom. The default is false.
+    #FIXME universalaccess.closeViewScrollWheelToggle = true;
+
+    # The filesystem path to which screencaptures should be written.
+    #FIXME screencapture.location = "${HOME}/Pictures/Screenshots";
+  };
 }
