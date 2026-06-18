ea410f0833c03eb3e41d68d351f9c9240d42d750
Emily Lockhart <validitymaiden@gmail.com>
2026-06-18 02:38:01 -0400

    feat(athena-vm): ✨ enable GNOME

--- hosts/athena-nixos/default.nix
+++ hosts/athena-nixos/default.nix
@@ -19,6 +19,14 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJMDk7MyQ+p48rILdmYb9A1VJwvpHrRgJReLpT0LnND5"
   ];
 
+  # GNOME https://wiki.nixos.org/wiki/GNOME
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.gnome.core-apps.enable = true;
+  services.gnome.core-developer-tools.enable = true;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
+
   networking.hostName = "athena-nixos";
 
   system.stateVersion = "24.11"; # Did you read the comment?
