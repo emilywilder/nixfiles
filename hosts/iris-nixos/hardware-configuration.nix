9c5a04c82940f5cf65090470f7fb4638a88878f9
Emily Lockhart <validitymaiden@gmail.com>
2026-06-23 16:04:19 -0400

    fix(iris-nixos): ⚡️ fix vulkan not using radeon gpu
    - radeon driver unable to find openssl
    - add path to openssl lib globally
    - not a good fix, but the only solution found thus far

--- hosts/iris-nixos/hardware-configuration.nix
+++ hosts/iris-nixos/hardware-configuration.nix
@@ -12,5 +12,16 @@
 
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
   };
+
+  # Fix for d3d12. the windows radeon driver requires openssl
+  # Source: https://github.com/nix-community/NixOS-WSL/issues/454#issuecomment-3802332145
+  environment.sessionVariables = {
+    LD_LIBRARY_PATH = [
+      "/run/opengl-driver/lib"
+      "${pkgs.openssl.out}/lib"
+    ];
+    GALLIUM_DRIVER = "d3d12";
+    };
 }
