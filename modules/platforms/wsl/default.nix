c4d6adc7d8bff3986079322a62b0046f088d9389
Emily Lockhart <validitymaiden@gmail.com>
2026-07-06 16:13:41 -0400

    docs(iris-nixos): 🎨 add context to wsl config options

--- modules/platforms/wsl/default.nix
+++ modules/platforms/wsl/default.nix
@@ -13,20 +13,31 @@
   # WSL
   # Docs: https://nix-community.github.io/NixOS-WSL/options.html
 
-  # Whether to enable support for running NixOS as a WSL distribution.
+  # Whether to enable support for running NixOS as a WSL distribution. (Default: false)
   wsl.enable = true;
-  # The name of the default user for WSL.
+
+  # The name of the default user. (Default: "nixos")
   wsl.defaultUser = config.my.username;
-  # Whether to enable OpenGL driver from the Windows host.
+
+  # Whether to enable OpenGL driver from the Windows host. (Default: false)
   wsl.useWindowsDriver = true;
-  # A command to run when the distro is started.
-  #wsl.wslConf.boot.command = "ip address flush dev eth0";
-  # Generate /etc/resolv.conf through WSL.
-  #wsl.wslConf.network.generateResolvConf = false;
+
+  # Configuration values for /etc/wsl.conf. See
+  # https://learn.microsoft.com/en-us/windows/wsl/wsl-config#configuration-settings-for-wslconf
+  # for all options supported by WSL.
+#  wsl.wslConf = {
+#    # A command to run when the distro is started. (Default: "")
+#    boot.command = "ip address flush dev eth0";
+#    # Generate /etc/resolv.conf through WSL. (Default: false)
+#    network.generateResolvConf = false;
+#  };
+
   # Whether to enable shortcuts for GUI applications in the windows start menu. (Default: false)
   wsl.startMenuLaunchers = true;
+
   # Let windows handle firewall in mirrored mode.
   networking.firewall.enable = false;
+
   # Disable wpa_supplicant
   networking.wireless.enable = lib.mkForce false;
 }
